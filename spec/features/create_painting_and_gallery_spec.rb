1aaaa8687dda1e516f740e7eec96eecec54c5cda
Zhengquan Yang <yangzhengquan@gmail.com>
2014-03-01 12:59:02 +0800

    add the description to the views

--- spec/features/create_painting_and_gallery_spec.rb
+++ spec/features/create_painting_and_gallery_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 feature "Create Painting for gallery" do
+  background do
+    Gallery.create(:name => "test")
+  end
   scenario "create a gallery" do
     visit "/galleries/new"
 
@@ -8,7 +11,21 @@ feature "Create Painting for gallery" do
       fill_in "Name", :with => 'gallery'
       fill_in "Description", :with => 'test gallery'
     end
-    click_link "Create Gallery"
+    click_button "Create Gallery"
+
     expect(page).to have_content "Successfully created gallery"
+  end
+
+  scenario "create a painting for a gallery" do
+    visit "/galleries/#{Gallery.first.id}"
+    click_link "Add a Painting"
+
+    within("#new_painting") do
+      fill_in "Name", :with => 'painting'
+      fill_in "Description", :with => 'painting description'
+    end
+    click_button "Create Painting"
+
+    expect(page).to have_content "Successfully created painting"
   end
 end
